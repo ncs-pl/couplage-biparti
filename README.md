78cdb06745c6ca4b17b6d64ee9027148bb27fd23
Nicolas Paul <nicolas.paul1@etu.univ-orleans.fr>
2025-11-18 16:38:26 +0100

    Move to GPL and prepapre files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Projet : calcul d'un couplage dans un graphe biparti
 
+Code en rapport de réponse dans le cadre du cours de *Modélisation,
+Graphes et Algorithmes* en Master 1 de l'Université d'Orléans.
+
+Binôme :
+- PAUL Nicolas <nicolas.paul1@etu.univ-orleans.fr>
+- AKKAYA Tolunay <tolunay.akkaya@etu.univ-orleans.fr>
+
---
+++ hopcroftkarp.ml
@@ -0,0 +1,21 @@
+
+(* Une implémentation de l'algorithme de Hopcroft-Karp en Objective Caml.
+   Copyright (C) 2025 Nicolas Paul <nicolas.paul1@etu.univ-orleans.fr> and
+   Tolunay Akkaya <tolunay.akkaya@etu.univ-orleans.fr>.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*)
+
+let () = ;;
+
